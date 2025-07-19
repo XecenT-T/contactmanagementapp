81a2f3914a53efcdbf2bd94d1a3fc8a83986b7b8
NIschay <nischaybagari@gmail.com>
2025-07-19 20:28:42 +0530

    fixed app

--- api/myserver.js
+++ api/myserver.js
@@ -7,16 +7,17 @@ app.use(cors())
 dbm();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'build')));
 
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 app.use(express.json())
 app.use("/api/contacts",require("./routes/contactroutes"))
 app.use("/api/users",require("./routes/userroutes"))
 app.use(require("./middleware/errorhandler"))
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const port=process.env.PORT || 3001
 app.listen(port,()=>{
